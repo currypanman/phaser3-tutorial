d7ae2cb5d25878ca4cd19f7177a5a85af70e1177
Currypanman <currypanman.spicy@outlook.com>
2023-04-13 12:50:05 +0900

    Add a sample code and use it from index.html. Move tutorial to tutorial.html.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   mode: 'development',
+  entry: {
+    index: './src/index.js',
+    tutorial: './src/tutorial.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+  },
   devServer: {
     static: 'dist',
     open: true
