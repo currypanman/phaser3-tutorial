988a1293771456b9a563888fd2568b51e1bbd62e
Currypanman <currypanman.spicy@outlook.com>
2023-04-18 06:58:14 +0900

    Implement a simple demo.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import 'phaser';
+import { GameObjects } from 'phaser';
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
@@ -17,29 +18,69 @@ const config: Phaser.Types.Core.GameConfig = {
 };
 
 const game: Phaser.Game = new Phaser.Game(config);
+let fruits: Phaser.Physics.Arcade.Group;
 
-function preload() {
-  this.load.setBaseURL('http://labs.phaser.io');
+function preload(this: Phaser.Scene) {
+  this.load.image('sky', 'assets/sky.png');
 
-  this.load.image('sky', 'assets/skies/space3.png');
-  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-  this.load.image('red', 'assets/particles/red.png');
+  this.load.image('orange', 'assets/fruit_orange.png');
+  this.load.image('apple', 'assets/fruit_ringo.png');
+  this.load.image('strawberry', 'assets/fruit_strawberry.png');
+  this.load.image('sumomo', 'assets/fruit_sumomo.png');
+  this.load.image('younashi', 'assets/fruit_younashi.png');
+  this.load.image('kiwi', 'assets/fruit_kiwi_marugoto.png');
+
+  this.load.image('boy', 'assets/stand1_front01_boy.png');
 }
 
-function create() {
+function eatFruit(this: Phaser.Scene, boy: Phaser.Physics.Arcade.Sprite, fruit: Phaser.Physics.Arcade.Sprite) {
+  fruit.disableBody();
+  this.tweens.add({
+    targets: fruit,
+    scale: 0,
+    duration: 500,
+    onComplete: () => {
+      fruit.disableBody(true, true);
+      if (fruits.countActive(true) == 0) {
+        let x = 100;
+        fruits.children.iterate(fruit => {
+          if (fruit instanceof Phaser.Physics.Arcade.Sprite) {
+            fruit.enableBody(true, x, 150, true, true);
+            this.tweens.add({
+              targets: fruit,
+              scale: 100 / fruit.width,
+              duration: 250
+            });
+            x += 150;
+          }
+          return true;
+        })
+      }
+    }
+  });
+}
+
+function create(this: Phaser.Scene) {
   this.add.image(400, 300, 'sky');
 
-  const emitter = this.add.particles(0, 0, 'red', {
-    speed: 100,
-    scale: { start: 1, end: 0 },
-    blendMode: 'ADD'
-  });
+  const boy = this.physics.add.sprite(400, 450, 'boy');
+  boy.scale = 200 / boy.width;
+  boy.setCircle(100);
 
-  const logo = this.physics.add.image(400, 100, 'logo');
+  const assets = ['orange', 'apple', 'strawberry', 'younashi', 'kiwi'];
 
-  logo.setVelocity(150, 200);
-  logo.setBounce(1, 1);
-  logo.setCollideWorldBounds(true);
+  let fs = [];
+  let x = 100;
+  for (const asset of assets) {
+    const fruit = this.physics.add.sprite(x, 150, asset);
+    fruit.setInteractive({ draggable: true });
+    fruit.on('drag', (p: any, x: number, y: number) => fruit.setPosition(x, y));
+    fruit.scale = 100 / fruit.width;
+    fruit.setCircle(50);
+    fs.push(fruit);
+    x += 150;
+  }
 
-  emitter.startFollow(logo);
+  fruits = this.physics.add.group(fs);
+  this.physics.add.overlap(boy, fruits, eatFruit, null, this);
 }
